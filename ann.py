9ce3f1476ae989fcbb2cfabee6e091bd870a084c
AakanshaDhawan <aakansha2809@gmail.com>
2018-08-17 15:08:48 +0530

    ann building

--- ann.py
+++ ann.py
@@ -32,4 +32,31 @@ sc=StandardScaler()
 X_train=sc.fit_transform(X_train)
 X_test=sc.transform(X_test)
 
+#ann
+import keras
+#used to initialize ann
+from keras.models import Sequential
+# used to built layers
+from keras.layers import Dense
 
+#classifier class is used
+classifier=Sequential()
+
+#adding the input and hidden layer
+#function used
+#rectifier -hidden && sigmoid - output
+#output value=6 avg of X and Y (11+1)/2=6
+classifier.add(Dense(output_dim=6,init='uniform',activation='relu',input_dim=11))
+#second hidden layer
+classifier.add(Dense(output_dim=6,init='uniform',activation='relu'))
+#output layer-
+#binary outcome only 1 output
+#for more than 1 output softmax(sigmoid funcion which is for more than variable) function is used 
+classifier.add(Dense(output_dim=1,init='uniform',activation='sigmoid'))
+
+#compiling the ann
+#optimize-to find optimal weight algorithm used is adam (type of scothsatic gradient)
+#loss- to calculate cost function
+classifier.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
+#fitting the ann-making connection between input and ann
+classifier.fit(X_train,y_train,batch_size=10,nb_epoch=100)
