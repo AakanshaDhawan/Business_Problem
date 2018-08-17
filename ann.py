ffa3dcfe4b36092e568f64d8dbff30df93a662ba
AakanshaDhawan <aakansha2809@gmail.com>
2018-08-17 15:08:15 +0530

    feature scaling

--- ann.py
+++ ann.py
@@ -25,3 +25,11 @@ X=X[:,1:]
 #Splitting the dataset into train and test
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+#feature scaling
+from sklearn.preprocessing import StandardScaler
+sc=StandardScaler()
+X_train=sc.fit_transform(X_train)
+X_test=sc.transform(X_test)
+
+
